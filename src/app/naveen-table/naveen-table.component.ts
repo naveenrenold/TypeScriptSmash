aff51aeead635e93577e7d19127ac025b0dc01e3
naveenrenold <naveenrenold@gmail.com>
2023-10-10 23:18:52 +0530

    commit final

--- src/app/naveen-table/naveen-table.component.ts
+++ src/app/naveen-table/naveen-table.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { tableData } from '../models/tableData';
+import { TableLazyLoadEvent, TablePageEvent } from 'primeng/table';
 
 @Component({
   selector: 'app-naveen-table',
@@ -8,12 +9,19 @@ import { tableData } from '../models/tableData';
 })
 export class NaveenTableComponent implements OnInit{
 tableDatas :tableData[]=[];
+tempTableDatas :tableData[]=[];
 
 ngOnInit(): void {
   this.tableDatas=[new tableData('Naveen',new Date(),25)
 ,
 new tableData('Renold',new Date(),18),
 new tableData('J',new Date(),5)
-]
+];
+this.tempTableDatas=[this.tableDatas[0]];
+}
+setTempTableData(event:TableLazyLoadEvent):void
+{
+console.log(`First:${event.first},Rows:${event.rows}`);
+this.tempTableDatas=[this.tableDatas[event.first??0]];
 }
 }
